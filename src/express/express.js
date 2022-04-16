fe62a71c4968d93ea49f7167e28581dfbdf78bf3
Dmitry Babaev <ba2evd@gmail.com>
2022-04-16 20:45:32 +0300

    module3-task2: prepare files for templates

--- src/express/express.js
+++ src/express/express.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 const articlesRoutes = require(`./routes/articles-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
@@ -12,5 +13,8 @@ const app = express();
 app.use(`/articles`, articlesRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT);
